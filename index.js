5533c8812c7e86b777cfa252df58a1b74de06612
Zhibin Dai <zhibindai26@gmail.com>
2020-01-28 20:29:24 -0500

    create csv and download

--- index.js
+++ index.js
@@ -22,8 +22,51 @@ var app = new Vue({
       .then(response => response.json())
       .then(data => {
         this.yelpData = data.body;
-        console.log(this.yelpData);
       })
+
+      const dataObject = this.yelpData.map(row => ({
+        name: row.name,
+        url: row.url,
+        categories: row.categories.map(category => category.title),
+        rating: row.rating,
+        review_count: row.review_count,
+        display_phone: row.display_phone,
+        address: row.Address1
+      }));
+      
+      console.log(dataObject);
+
+      const objToCSV = function(data) {
+        const csvRows = [];
+        const headers = Object.keys(data[0]);
+        csvRows.push(headers.join(','));
+
+        for (const row of data) { 
+          const values = headers.map(header => {
+            const escaped = (''+row[header]).replace(/"/g, '\\"');
+            return `"${escaped}"`;
+          });
+         csvRows.push(values.join(',')); 
+        }
+        const output = csvRows.join('\n');
+        return output;
+      }
+
+      const download = function(data, filename) {
+        const blob = new Blob([data], {type: 'text/csv'});
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.setAttribute('hidden', '');
+        a.setAttribute('href', url);
+        a.setAttribute('download', filename);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
+
+      const csvData = objToCSV(dataObject);
+      download(csvData, 'download.csv');
     }
   }
 })
+
